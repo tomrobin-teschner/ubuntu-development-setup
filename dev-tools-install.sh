e5aa31d6a96631e4fef5150c030319bd8b94bccb
Tom-Robin Teschner <tomrobin.teschner@yahoo.de>
2020-12-23 20:31:29 +0000

    Added error trap
    If the current command fails, an error message will be printed
    to the screen and the executation is interrupted.
    Some helpful debug message will also be printed, showing
    where and what went wrong.

--- dev-tools-install.sh
+++ dev-tools-install.sh
@@ -6,6 +6,15 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 # get latest updates
 sudo apt-get -y update
 
