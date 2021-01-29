e8835a7eee806cb81c402763e10b21e69addf7dd
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2021-01-29 22:44:28 +0000

    Added nlohmann's JSON implementation

--- dev-tools-install.sh
+++ dev-tools-install.sh
@@ -86,7 +86,7 @@ eb --trace --robot --try-toolchain-version=2020b easyconfig/HDF5-1.12.0-gompi-20
 eb --trace --robot easyconfig/CGNS-4.1.0-gompi-2020b.eb
 eb --trace --robot easyconfig/googletest-1.10.0-GCCcore-10.2.0.eb
 eb --trace --robot Doxygen-1.8.20-GCCcore-10.2.0.eb
-eb --trace --robot JsonCpp-1.9.4-GCCcore-10.2.0.eb
+eb --trace --robot easyconfig/JSONForModernC++-3.9.1-GCCcore-10.2.0
 eb --try-toolchain-version=2020b --trace --robot Valgrind-3.16.1-gompi-2020a.eb
 
 # additional developer libraries and tools installed through pip
